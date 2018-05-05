d8247fbe07ddb12581fe490301bc8d7b425b5855
Sutean Rutjanalard <sutean@siamsquared.com>
2018-05-05 15:11:12 +0700

    Add fail test

--- test_api/main/main.js
+++ test_api/main/main.js
@@ -3,6 +3,18 @@ var expect = chakram.expect;
 var base = "http://localhost:8080/";
 
 describe("task/", function () {
+    var taskSchema;
+    before(function () {
+        taskSchema = {
+            "type": "object",
+            properties: {
+                id: { type: "number" },
+                name: { type: "string" },
+                completed: { type: "boolean" },
+                due: { type: "string" }
+            }
+        };
+    });
     it("should provide list of existing tasks", function () {
         var response = chakram.get(base + "/task")
         expect(response).to.have.status(200);
@@ -15,17 +27,7 @@ describe("task/", function () {
     it("should provide an item when given an existing id", function () {
         var response = chakram.get(base + "task/1")
         expect(response).to.have.status(200);
-        expect(response).to.have.schema(
-            {
-                "type": "object",
-                properties: {
-                    id: { type: "number" },
-                    name: { type: "string" },
-                    completed: { type: "boolean" },
-                    due: { type: "string" }
-                }
-            }
-        )
+        expect(response).to.have.schema(taskSchema);
         return chakram.wait();
     });
 
@@ -37,22 +39,30 @@ describe("task/", function () {
         };
         var response = chakram.post(base + "task", body);
         expect(response).to.have.status(200);
-        expect(response).to.have.schema(
-            {
-                "type": "object",
-                properties: {
-                    id: { type: "number" },
-                    name: { type: "string" },
-                    completed: { type: "boolean" },
-                    due: { type: "string" }
-                }
-            }
-        )
+        expect(response).to.have.schema(taskSchema);
+        
         expect(response).to.have.json('id', function(id) {
             expect(id).to.be.above(2);
         })
         return chakram.wait();
     })
 
+    it("should update exiting task with new title", function() {
+        let updateName = "Update task name";
+        body = {
+            id: 1,
+            name: updateName,
+            completed: false,
+            due: new Date()
+        };
+        var response = chakram.put(base + "task", body);
+        expect(response).to.have.status(200);
+        response = chakram.get(base+"task/1")
+        expect(response).to.have.status(200);
+        expect(response).to.have.json('name', function(name) {
+            expect(name).to.equal(updateName);
+        })
+        return chakram.wait();
+    })
     
 });
